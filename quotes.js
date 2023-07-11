d24e9a7f88311492560e4a013943980a0c580feb
Zak Franczak <zakfranczak@gmail.com>
2023-07-11 16:17:57 -0500

    moved QUOTEBANK to own file

---
+++ quotes.js
@@ -0,0 +1,28 @@
+const QUOTEBANK = [
+  {
+    quote: 'Be yourself; everyone else is already taken.',
+    author: 'Oscar Wilde',
+  },
+  {
+    quote:
+      "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+    author: 'Marilyn Monroe',
+  },
+  {
+    quote: 'So many books, so little time.',
+    author: 'Frank Zappa',
+  },
+  {
+    quote: 'A room without books is like a body without a soul',
+    author: 'Frank Zappa',
+  },
+  {
+    quote: 'So many books, so little time.',
+    author: 'Marcus Tullius Cicero',
+  },
+  {
+    quote:
+      "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+    author: 'Bernard M. Baruch',
+  },
+];
